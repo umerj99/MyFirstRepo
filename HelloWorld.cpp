317a035c113a3b19ba6be4938e239f2c9dd49879
umerj99 <109678016+umerj99@users.noreply.github.com>
2022-07-20 16:39:07 +0200

    Included "add" function with output in main

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
-
+int add (int a, int b) {
+  int c = a + b;
+  return c
+    
 int main(){
   std::cout << "Hello, World!" << std:endl
     << "How are you doing?" << std::endl;
+  std::cout << "3 + 5 is " << add(int a = 3, int b =5) << std::endl;
 }
 
