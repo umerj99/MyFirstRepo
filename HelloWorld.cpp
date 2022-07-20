3a0842b7dd235bdf4c0099e80940c0e308699c08
umerj99 <109678016+umerj99@users.noreply.github.com>
2022-07-20 17:29:10 +0200

    Update HelloWorld.cpp

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -8,11 +8,17 @@ float subtract (float d, float e){
     float subtract = d - e;
     return subtract;
 }
+
+double divide(double h, double i){
+     double divide = h/i;
+     return divide;
+}
     
 int main(){
   std::cout << "Hello, World!" << std::endl
     << "How are you doing?" << std::endl;
   std::cout << "3 + 5 is " << add(3, 5) << std::endl
-   << "3.0- 5.0 is " << subtract (3.0, 5.0) << std::endl;
+   << "3.0- 5.0 is " << subtract (3.0, 5.0) << std::endl
+   << "3.0/5.0 is" << divide(3.0,5.0) << std::endl;
 }
 
