db9e6eb0eb1c8650bcfbaac245a4d79f3eb8951a
umerj99 <109678016+umerj99@users.noreply.github.com>
2022-07-20 17:07:42 +0200

    Update HelloWorld.cpp

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -3,10 +3,16 @@ int add (int a, int b) {
   int c = a + b;
   return c;
 }
+
+float subtract (float d, float e){
+    float subtract = d - e;
+    return subtract;
+}
     
 int main(){
   std::cout << "Hello, World!" << std::endl
     << "How are you doing?" << std::endl;
-  std::cout << "3 + 5 is " << add(3, 5) << std::endl;
+  std::cout << "3 + 5 is " << add(3, 5) << std::endl
+   << "3.0- 5.0 is " << subtract (3.0, 5.0) << std::endl;
 }
 
