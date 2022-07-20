04186084cb1e18f7808e909c6a037a8cea84fec2
umerj99 <109678016+umerj99@users.noreply.github.com>
2022-07-20 16:54:33 +0200

    fixed some bugs. code woks now

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
 int add (int a, int b) {
   int c = a + b;
-  return c
+  return c;
 }
     
 int main(){
-  std::cout << "Hello, World!" << std:endl
+  std::cout << "Hello, World!" << std::endl
     << "How are you doing?" << std::endl;
-  std::cout << "3 + 5 is " << add(int a = 3, int b =5) << std::endl;
+  std::cout << "3 + 5 is " << add(3, 5) << std::endl;
 }
 
