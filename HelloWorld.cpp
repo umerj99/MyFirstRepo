549b5aff611128a48b4ea088ce000ac5e6e23e27
umerj99 <109678016+umerj99@users.noreply.github.com>
2022-07-20 17:26:43 +0200

    Adding multiply func
    also printing it in main function

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -8,11 +8,17 @@ float subtract (float d, float e){
     float subtract = d - e;
     return subtract;
 }
+
+double multiply (double f, double g){
+     double multiply = f*g;
+     return multiply;
+}
     
 int main(){
   std::cout << "Hello, World!" << std::endl
     << "How are you doing?" << std::endl;
   std::cout << "3 + 5 is " << add(3, 5) << std::endl
-   << "3.0- 5.0 is " << subtract (3.0, 5.0) << std::endl;
+   << "3.0 - 5.0 is " << subtract (3.0, 5.0) << std::endl
+   << "3.0*5.0 is" << multiply(3.0,5.0) << std::endl;
 }
 
